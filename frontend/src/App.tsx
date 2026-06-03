1fe7c651550172b937807727303bba04db951b85
mli57 <maxwell.l4991@gmail.com>
2026-06-03 15:32:30 +0800

    First working fullstack implementation of CounterPick

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,34 @@
+import { useState } from "react";
+
+type PredictResult = {
+    win_probability: number;
+    warnings: string[];
+}
+
 export default function App() {
-    return <div>CounterPick UI</div>
+    const [champion, setChampion] = useState("");
+    const [opponent, setOpponent] = useState("");
+    const [role, setRole] = useState("");
+    const [result, setResult] = useState<PredictResult | null>(null);
+
+    async function fetchChampionData(){
+        const response = await fetch("http://localhost:8000/predict", { 
+            method: "POST",
+            headers: { "Content-Type": "application/json" }, //send champion, opponent, role as JSON
+            body: JSON.stringify({ champion, opponent, role})
+        });
+        const data = await response.json();
+        setResult(data);
+    }
+    return(
+        <div>
+            CounterPick UI
+            <input placeholder="Enter your champion" value={champion} onChange={(e) => setChampion(e.target.value)}/>
+            <input placeholder="Enter opponent champion" value={opponent} onChange={(e) => setOpponent(e.target.value)}/>
+            <input placeholder="Enter role" value={role} onChange={(e) => setRole(e.target.value)}/>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={fetchChampionData}>fetch champion data</button>
+            
+            {result && <p>{result.win_probability}</p>}
+        </div>
+    );
 }
